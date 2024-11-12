dc83356306fbaeb564717f971a1b71d9b32ff85a
dayako24 <dayakofarrux@gmail.com>
2024-11-12 07:29:15 +0500

    hw 2

---
+++ homework2.py
@@ -0,0 +1,162 @@
+# a=float(input())
+# b=round(a, 2)
+# print(b)
+
+# a=float(input())
+# b=float(input())
+# c=float(input())
+# d=max(a, b, c)
+# e=min(a, b, c)
+# print(f"{d}, {e}")
+
+# a=float(input())
+# b=int(a*1000)
+# c=round((a*1000-b)*100)
+# print(f"{a} kilometers equals to {b} metres and {c} centimeters")
+
+# a=float(input())
+# b=float(input())
+# print(divmod(a, b))
+
+# a=float(input())
+# print(f"{a} celcius equlas to {(a*9/5)+32} f")
+
+# a=str(input())
+# b=len(a)
+# print(a[b-1])
+
+# a=int(input())
+# if a%2==0:
+#     print("even")
+# else: 
+#     print("odd")
+
+# a=float(input("a="))
+# b=float(input("b="))
+# print(f"a={b}, b={a}")
+
+# a=str(input("enter a sentence: "))
+# print(a.title())
+
+# a=str(input("enter a word: "))
+# b=a
+# vowels="aeiouAEIOU"
+# for x in vowels:
+#     b=b.replace(x, "")
+# print(b)
+
+# a=str(input("enter a sentence: "))
+# b=len(a)
+# c=str()
+# for i in range (b-1, -1, -1):
+#     c+=a[i]
+# print(c)
+
+# a=str(input("enter a sentence: "))
+# b=a.replace(" ", "_")
+# print(b)
+
+
+# a=str(input("enter a sentence: "))
+# b=len(a)
+# c=str()
+# for i in range (b-1, -1, -1):
+#     c+=a[i]
+# print(c==a)
+
+# a=str(input("enter a sentence: "))
+# b=a.replace("a", "o")
+# print(b)
+
+# a=str(input("enter a email: "))
+# b=a.find("@")
+# print(a[:b])
+
+# a=str(input("enter a semtemce: "))
+# print(a.title)
+
+# a=str(input("enter a semtemce: "))
+# print(c[1:-1])
+
+# a=str(input("enter a semtemce: "))
+# b=str(input("enter a word: "))
+# def checks(sentence, word):
+#     for b in a.split():
+#         return True
+#     else:
+#         return False
+# if checks(a, b):
+#     print("true")
+# else:
+#      print("false")    
+
+# a=str(input("enter a str: "))
+# b=str(input("enter  a character: "))
+# c=a.find(b)
+# print(c)
+
+# a=str(input("enter a str: "))
+# b=len(a)
+# print(a[:b-3])
+
+# a=str(input("enter a str: "))
+# b=int(input())
+# print(a*b)
+
+# a=str(input("enter a str: "))
+# b=a.replace(" ", "\n")
+# print(b)
+
+# a=str(input("enter a str: ")) 
+# print(a[1::2])
+
+# a=str(input("enter a sentence: "))
+# b=a.split()
+# d=" ".join(reversed(b))
+# print(d)
+
+# a=str(input("enter a sentence: "))
+# b=str(input("enter a sentence: "))
+# print(abs(len(a)-len(b)))
+
+# a=str(input("enter a username: "))
+# b=str(input("enter a password: "))
+# print(a==b)
+
+# a=float(input())
+# b=float(input())
+# print(a==b)
+
+# a=int(input())
+# if a%2==0 :
+#     print("even")
+# else:
+#     print('odd')
+
+# a=str(input("enter a sentence: "))
+# b=str(input("enter a sentence: "))
+# print(len(a)==len(b))
+
+# a=int(input())
+# if a%15==0 :
+#     print("yes")
+# else:
+#     print('no')
+
+# a=int(input())
+# b=int(input())
+# print(a=b>50)
+
+# a=int(input())
+# if a-10>=0:
+#     if 20-a>=0:
+#         print("yes")
+#     else: 
+#         print("no")
+# else:
+#     print("no")
+
+
+
+
+
